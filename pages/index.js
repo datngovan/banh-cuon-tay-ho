83c6c4ed136ae822f4186d26c11ac655711a029a
datngovan <s3817813@rmit.edu.vn>
2023-12-30 18:43:50 +0700

    feat: test deploy

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,21 @@ import History from "@/components/layouts/history";
 import NavigationBar from "@/components/layouts/navigation-bar";
 import Menu from "@/components/menu";
 import { Fragment } from "react";
-import { getFeaturedPosts, getVegetarianFeaturedPosts, getNonVegetarianFeaturedPosts } from "@/lib/utils";
+import { getFeaturedPosts} from "@/lib/utils";
 export default function Home(props) {
   return (
     <Fragment>
       <Cover/>
       <History/>
-      <Menu props={props.posts}/>
+      {/* <Menu props={props.posts}/> */}
     </Fragment>
   );
 }
-export function getStaticProps(){
-  const featuredPosts = getFeaturedPosts();
-  return {
-      props:{
-          posts: featuredPosts
-      }
-  }
-}
+// export function getStaticProps(){
+//   const featuredPosts = getFeaturedPosts();
+//   return {
+//       props:{
+//           posts: featuredPosts
+//       }
+//   }
+// }
