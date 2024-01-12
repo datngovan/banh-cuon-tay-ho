f00faf6c64212a60de23db5f8ba4a91329860125
datngovan <s3817813@rmit.edu.vn>
2024-01-12 17:29:14 +0700

    update: update tab-button visual

--- components/tab-button.js
+++ components/tab-button.js
@@ -3,11 +3,15 @@ import Link from "next/link";
 export default function TabButtons(props) {
     if(props.actived !== "true"){
       return (
-        <Link className="m-auto p-3 text-2xl border-primary-50 border-2 rounded-full text-primary-50 font-medium hover:bg-primary-50 hover:text-white active:bg-primary-90 active:text-white" href={props.link}>{props.children}</Link>
+          <div className=" text-center m-auto p-3 text-2xl border-primary-50 border-2 rounded-full text-primary-50 font-medium hover:bg-primary-50 hover:text-white active:bg-primary-90 active:text-white w-4/12 ">
+            <Link href={props.link}>{props.children}</Link>
+          </div>
       )
     }
     return (
-      <Link className="m-auto p-3 text-2xl border-primary-50 border-2 rounded-full text-white bg-primary-50  aria-disabled" href={props.link}>{props.children}</Link>
+      <div className="text-center m-auto p-3 text-2xl border-primary-50 border-2 rounded-full text-white bg-primary-50 aria-disabled w-4/12">
+          <Link href={props.link}>{props.children}</Link>
+      </div>
     )
   }
   
--- pages/menu/mon-man/index.js
+++ pages/menu/mon-man/index.js
@@ -5,14 +5,14 @@ export default function NonVegetarianMenuPage(props){
     return<><div className="pt-20 text-center font-bold text-primary-50 text-2xl">
         THỰC ĐƠN
     </div><div>
-            <div className="flex flex-row items-center justify-around mt-10 p-10">
+            <div className="grid grid-cols-1 md:grid-cols-3 items-center justify-around mt-10 p-10">
                 <TabButtons name="TẤT CẢ" id="all" value="TẤT CẢ" link="/menu">TẤT CẢ</TabButtons>
                 <TabButtons name="BÁNH MẶN" id="nonVegetarian" link="/menu/mon-man" actived="true" value="BÁNH MẶN">BÁNH MẶN</TabButtons>
                 <TabButtons name="BÁNH CHAY" id="Vegetarian" link="/menu/mon-chay" value="BÁNH CHAY">BÁNH CHAY</TabButtons>
             </div>
     </div>
     <div>
-    <ul className="flex flex-row justify-around p-16 flex-wrap lg:gap-3">
+    <ul className="grid grid-cols-4 justify-around p-16 flex-wrap lg:gap-3">
     {props.items.NonVegetarian.map(item=>
         (<li key={item.slug}><ItemGrid item={item}/></li>)
     )}
