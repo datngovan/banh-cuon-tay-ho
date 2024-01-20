b98778635813e0ac074529ffc112cfd21352981e
datngovan <s3817813@rmit.edu.vn>
2024-01-20 13:56:58 +0700

    feat: update sitemap

--- components/layouts/cover.js
+++ components/layouts/cover.js
@@ -3,7 +3,7 @@ import NavigationBarV2 from "./navigation-bar-v2";
 function Cover(){
     return <div className="static">
         <NavigationBarV2 className="z-[1]"/>
-        <div className="min-h-svh md:h-lvh min-h-svh mb-[-20] md:min-h-dvh md:bg-center bg-no-repeat bg-[url('/../image/banner.png')] md:bg-[length:100vw_100vh] bg-[length:100vw_80vh]">
+        <div className="bg-white min-h-svh md:min-h-lvh mb-[-20] md:min-h-dvh md:bg-center bg-no-repeat bg-[url('/../image/banner.png')] md:bg-[length:100vw_100vh] bg-[length:100vw_80vh]">
         </div>
     </div>
 }
--- pages/_app.js
+++ pages/_app.js
@@ -8,9 +8,9 @@ export default function App({ Component, pageProps }) {
   return <Fragment>
   <Head>
 
-        <title>Bánh Cuốn Tây Hồ</title>
+        <title>Bánh Cuốn Tây Hồ - Đinh Tiên Hoàng - Thành Phố Hồ Chí Minh</title>
         <meta name="title" content="Bánh Cuốn Tây Hồ - Đinh Tiên Hoàng - Thành Phố Hồ Chí Minh"/>
-        <link rel="icon" href="https://restcipe.io.vn/favicon.ico"></link>
+        <link rel="icon" href="https://restcipe.io.vn/favicon.ico" type="image/x-icon"></link>
         <meta name="description" content="Bánh Cuốn Tây Hồ được Bà Trần Thị Cà (1919-2006) quê ở Hà Nội mở năm 1962 tại sân đến thờ Cụ Phan Châu Trinh hiệu là Tây Hồ lúc ấy đấu chợ Đa Kao, Quận 1"/>
         <meta property="og:image" content="https://restcipe.io.vn/opengraph-image.png"/>
         <meta property="og:url" content="https://restcipe.io.vn" />
